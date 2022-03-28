f6c3ed592542e8bb4547c8a01f9c856e9e1a2593
rabinbhat <bhatravi984@gmail.com>
2022-03-28 14:03:06 +0545

    initial commit

---
+++ public/src/components/Curriencies.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Curriencies = () => {
+  return <div>Curriencies</div>;
+};
+
+export default Curriencies;
---
+++ public/src/components/index.js
@@ -0,0 +1,6 @@
+export { default as Homepage } from "./Homepage";
+export { default as Navbar } from "./Navbar";
+export { default as Exchanges } from "./Exchanges";
+export { default as Curriencies } from "./Curriencies";
+export { default as News } from "./News";
+export { default as CryptoDetails } from "./Homepage";
